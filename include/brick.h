e485b9510a46ee488965ed9cc8d1f4abdec2a78c
avanip2 <avanip2@illinois.edu>
2021-04-20 22:14:31 -0500

    Added setters and getters to brick class

--- include/brick.h
+++ include/brick.h
@@ -9,6 +9,14 @@ class Brick {
  public:
   Brick(size_t set_num_hits, vec2 set_top_left_position, vec2 set_bottom_right_position, ci::Color set_color);
   void DisplayBrick() const;
+  void SetNumHits(size_t set_num_hits);
+  size_t GetNumHits();
+  void SetTopLeftPosition(vec2 set_top_left_position);
+  vec2 GetTopLeftPosition();
+  void SetBottomRightPosition(vec2 set_bottom_right_position);
+  vec2 GetBottomRightPosition();
+  void SetColor(ci::Color set_color);
+  ci::Color GetColor();
  private:
   size_t num_hits_;
   vec2 top_left_position_;
--- src/brick.cpp
+++ src/brick.cpp
@@ -2,6 +2,9 @@
 #include "cinder/gl/gl.h"
 
 namespace brickbreaker {
+
+using glm::vec2;
+
 Brick::Brick(size_t set_num_hits, vec2 set_top_left_position, vec2 set_bottom_right_position, ci::Color set_color) {
   num_hits_ = set_num_hits;
   top_left_position_ = set_top_left_position;
@@ -14,4 +17,35 @@ void Brick::DisplayBrick() const {
   ci::gl::drawSolidRect(ci::Rectf(top_left_position_, bottom_right_position_));
 }
 
+void Brick::SetColor(ci::Color set_color) {
+  color_ = set_color;
+}
+
+ci::Color Brick::GetColor() {
+  return color_;
+}
+
+void Brick::SetBottomRightPosition(vec2 set_bottom_right_position) {
+  bottom_right_position_ = set_bottom_right_position;
+}
+
+vec2 Brick::GetBottomRightPosition() {
+  return bottom_right_position_;
+}
+
+void Brick::SetTopLeftPosition(vec2 set_top_left_position) {
+  top_left_position_ = set_top_left_position;
+}
+
+vec2 Brick::GetTopLeftPosition() {
+  return top_left_position_;
+}
+
+void Brick::SetNumHits(size_t set_num_hits) {
+  num_hits_ = set_num_hits;
+}
+
+size_t Brick::GetNumHits() {
+  return num_hits_;
+}
 } //namespace brickbreaker
