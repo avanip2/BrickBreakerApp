7a8ec1709473597504153e3061ce8d0e939b071f
avanip2 <avanip2@illinois.edu>
2021-04-30 13:48:15 -0500

    Added test for brick number of hits setter

--- tests/brick_test.cpp
+++ tests/brick_test.cpp
@@ -4,3 +4,11 @@
 using glm::vec2;
 using brickbreaker::Brick;
 
+TEST_CASE("Brick number of hits is properly decrementing") {
+  Brick brick(5, vec2{50,50}, vec2{100,100}, ci::Color("blue"), 50);
+
+  SECTION("Decrease brick number of hits by 1") {
+    brick.SetNumHits(brick.GetNumHits() - 1);
+    REQUIRE(brick.GetNumHits() == 4);
+  }
+}
